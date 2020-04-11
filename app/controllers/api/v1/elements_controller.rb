762b4b3a97ad35fbb1c4dfecce33a46f18e18273
giancarloz <gzabaleta48@gmail.com>
2020-04-11 10:59:03 -0400

    add element and spot controller code

--- app/controllers/api/v1/elements_controller.rb
+++ app/controllers/api/v1/elements_controller.rb
@@ -1,2 +1,35 @@
 class Api::V1::ElementsController < ApplicationController
+
+    def index
+        @elements = Element.all
+
+        render json: @elements
+    end
+
+    def create
+        @element = Element.create(element_params)
+        render json: @elements
+    end
+
+    def show
+        @element = Element.find(params[:id])
+        render json: @elements
+    end
+
+    def update
+        @element = Element.find(params[:id])
+        @element.update(element_params)
+    end
+
+    def destroy 
+        @element = Element.find(params[:id])
+        @element.destroy
+    end
+
+    private
+
+    def element_params 
+        params.require(:element).permit(:name, :style, :level)
+    end
+
 end
--- app/controllers/api/v1/spots_controller.rb
+++ app/controllers/api/v1/spots_controller.rb
@@ -1,2 +1,35 @@
 class Api::V1::SpotsController < ApplicationController
+    def index
+        @spot = Spot.all
+
+        render json: @spot
+    end
+
+    def create
+        @spot = Spot.create(spot_params)
+        render json: @spot
+    end
+
+    def show
+        @spot = Spot.find(params[:id])
+        render json: @spot
+    end
+
+    def update
+        @spot = Spot.find(params[:id])
+        @spot.update(spot_params)
+        render json: @spot
+    end
+
+    def destroy 
+        @spot = Spot.find(params[:id])
+        @spot.destroy
+        render json: @spot
+    end
+
+    private
+
+    def spot_params 
+        params.require(:spot).permit(:name, :style, :material, :longitude, :latitude, :item_id)
+    end
 end
